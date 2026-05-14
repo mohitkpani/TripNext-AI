df2ab9159a3de6d150f8514d4a01cac518e86bbf
Mohit Kumar Pani <mohitpani007@gmail.com>
2026-05-14 10:35:21 +0530

    Fixed

--- src/main/java/com/tripnext/TripnextApplication.java
+++ src/main/java/com/tripnext/TripnextApplication.java
@@ -3,7 +3,7 @@ package com.tripnext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplicationgit 
+@SpringBootApplication
 public class TripnextApplication {
 
 	public static void main(String[] args) {
